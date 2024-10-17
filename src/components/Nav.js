b9c8d3a79adadd9ccbdc2507e0ce81885f53a354
munseongyun <borygashill608@gmail.com>
2024-10-17 10:03:00 +0900

    ✅ test : Nav.js 안에 onSearch 함수안에 검색 예제 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,20 @@ import { React, useState } from "react";
 import "../css/Nav.css";
 import { Link } from "react-router-dom";
 import searchIcon from "../img/search.jpg";
+import axios from '../lib/axios'
 const Nav = () => {
   const [searchInput, setSearchInput] = useState("");
 
   const onChange = (e) => {
     setSearchInput(e.target.value);
   };
-  const onSearch = () => {}; /*  */
+  const onSearch = (e) => {
+    e.preventDefault()
+    const content = searchInput.split(' ')
+    axios.get(`http://localhost:8000/api/search?target=tile&content=${content.join('-')}&category=자유게시판`)
+    .then(res=>console.log(res.data))
+    .catch((e)=>console.log(e.response.data.message))
+  }; /*  */
 
   return (
     <div className="parent">
@@ -18,12 +25,15 @@ const Nav = () => {
           <div className="logo"></div>
         </Link>
         <div className="rectangle">
+          <form onSubmit={onSearch}>
           <input
             className="search-box"
             placeholder="내용을 입력하세요."
             value={searchInput}
             onChange={onChange}
           />
+          </form>
+          
           <Link to="/search-result" state={{ searchInput: searchInput }}>
             <button className="search-icon">
               <img src={searchIcon} alt="" />
